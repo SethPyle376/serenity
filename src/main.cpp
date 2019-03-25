7b91910ba195955f066c437b814ea8174c0f6058
Seth Pyle <pyl13002@byui.edu>
2019-03-24 21:04:23 -0600

    Added ENTT into project. Removed old ECS

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "Engine/Application/Application.h"
+#include "Engine/thirdparty/entt/entt.hpp"
 
 int main()
 {
 	Application application;
 	application.process();
 	return 0;
+	entt::registry registry;
 }
